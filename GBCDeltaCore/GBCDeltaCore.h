a988eb608042068199709eb645d1f8915ea11d9b
Riley Testut <riley@rileytestut.com>
2017-04-06 15:43:18 -0700

    Initial Commit

---
+++ GBCDeltaCore/GBCDeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  GBCDeltaCore.h
+//  GBCDeltaCore
+//
+//  Created by Riley Testut on 4/10/17.
+//  Copyright © 2017 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GBCDeltaCore.
+FOUNDATION_EXPORT double GBCDeltaCoreVersionNumber;
+
+//! Project version string for GBCDeltaCore.
+FOUNDATION_EXPORT const unsigned char GBCDeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GBCDeltaCore/PublicHeader.h>
+
+
