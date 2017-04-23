80ca21b5bb17685e66a7f9741a1ece8952ac83f3
Riley Testut <riley@rileytestut.com>
2017-04-23 02:33:51 -0700

    Updates GBC.swift to match revised DeltaCoreProtocol

--- GBCDeltaCore/GBC.swift
+++ GBCDeltaCore/GBC.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import AVFoundation
+
+import DeltaCore
 
 public extension GameType
 {
@@ -17,13 +20,27 @@ public struct GBC: DeltaCoreProtocol
 {
     public static let core = GBC()
     
+    public let gameType = GameType.gbc
+    
     public let bundleIdentifier: String = "com.rileytestut.GBCDeltaCore"
     
-    public let supportedGameTypes: Set<GameType> = [.gbc]
+    public let gameSaveFileExtension: String = "sav"
+    
+    public let frameDuration = (1.0 / 59.7275)
+    
+    public let supportedRates: ClosedRange<Double> = 1...4
+    
+    public let supportedCheatFormats: [CheatFormat] = {
+        let gameGenieFormat = CheatFormat(name: NSLocalizedString("Game Genie", comment: ""), format: "XXX-YYY-ZZZ", type: .gameGenie)
+        let gameSharkFormat = CheatFormat(name: NSLocalizedString("GameShark", comment: ""), format: "XXXXXXXX", type: .gameShark)
+        return [gameGenieFormat, gameSharkFormat]
+    }()
+    
+    public let audioFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: 35112 * 59.7275, channels: 2, interleaved: true)
+    
+    public let videoFormat = VideoFormat(pixelFormat: .bgra8, dimensions: CGSize(width: 160, height: 144))
     
     public let emulatorBridge: EmulatorBridging = GBCEmulatorBridge.shared
-    
-    public let emulatorConfiguration: EmulatorConfiguration = GBCEmulatorConfiguration()
     
     public let inputTransformer: InputTransforming = GBCInputTransformer()
     
