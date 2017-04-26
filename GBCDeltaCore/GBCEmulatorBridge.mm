35b4911345be79d972dad4233bc91f6d05ba4eb2
Riley Testut <riley@rileytestut.com>
2017-04-26 00:05:10 -0700

    Implements Save States and Game Saves

--- GBCDeltaCore/GBCEmulatorBridge.mm
+++ GBCDeltaCore/GBCEmulatorBridge.mm
@@ -17,6 +17,7 @@
 @interface GBCEmulatorBridge ()
 
 @property (nonatomic, copy, nullable, readwrite) NSURL *gameURL;
+@property (nonatomic, copy, nonnull, readonly) NSURL *gameSaveDirectory;
 
 @property (nonatomic, assign, readonly) std::shared_ptr<gambatte::GB> gambatte;
 @property (nonatomic, assign, readonly) std::shared_ptr<GBCInputGetter> inputGetter;
@@ -44,11 +45,14 @@
     self = [super init];
     if (self)
     {
+        _gameSaveDirectory = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
+        
         std::shared_ptr<GBCInputGetter> inputGetter(new GBCInputGetter());
         _inputGetter = inputGetter;
         
         std::shared_ptr<gambatte::GB> gambatte(new gambatte::GB());
         gambatte->setInputGetter(inputGetter.get());
+        gambatte->setSaveDir(_gameSaveDirectory.fileSystemRepresentation);
         _gambatte = gambatte;
     }
     
@@ -120,26 +124,54 @@
 
 #pragma mark - Save States -
 
-- (void)saveSaveStateToURL:(NSURL *)url
+- (void)saveSaveStateToURL:(NSURL *)URL
 {
-    
+    self.gambatte->saveState(NULL, 0, URL.fileSystemRepresentation);
 }
 
-- (void)loadSaveStateFromURL:(NSURL *)url
+- (void)loadSaveStateFromURL:(NSURL *)URL
 {
-    
+    self.gambatte->loadState(URL.fileSystemRepresentation);
 }
 
 #pragma mark - Game Saves -
 
-- (void)saveGameSaveToURL:(NSURL *)url
+- (void)saveGameSaveToURL:(NSURL *)URL
 {
+    // Cannot directly set the URL for saving game saves, so we save it to the temporary directory and then move it to the correct place.
     
+    self.gambatte->saveSavedata();
+    
+    NSString *gameFilename = self.gameURL.lastPathComponent.stringByDeletingPathExtension;
+    NSURL *temporarySaveURL = [self.gameSaveDirectory URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.sav", gameFilename]];
+    
+    if ([self safelyCopyFileAtURL:temporarySaveURL toURL:URL])
+    {
+        NSURL *rtcURL = [[URL URLByDeletingPathExtension] URLByAppendingPathExtension:@"rtc"];
+        NSURL *temporaryRTCURL = [self.gameSaveDirectory URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.rtc", gameFilename]];
+        
+        if ([[NSFileManager defaultManager] fileExistsAtPath:temporaryRTCURL.path])
+        {
+            [self safelyCopyFileAtURL:temporaryRTCURL toURL:rtcURL];
+        }
+    }
 }
 
-- (void)loadGameSaveFromURL:(NSURL *)url
+- (void)loadGameSaveFromURL:(NSURL *)URL
 {
+    NSString *gameFilename = self.gameURL.lastPathComponent.stringByDeletingPathExtension;
+    NSURL *temporarySaveURL = [self.gameSaveDirectory URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.sav", gameFilename]];
     
+    if ([self safelyCopyFileAtURL:URL toURL:temporarySaveURL])
+    {
+        NSURL *rtcURL = [[URL URLByDeletingPathExtension] URLByAppendingPathExtension:@"rtc"];
+        NSURL *temporaryRTCURL = [self.gameSaveDirectory URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.rtc", gameFilename]];
+        
+        if ([[NSFileManager defaultManager] fileExistsAtPath:rtcURL.path])
+        {
+            [self safelyCopyFileAtURL:rtcURL toURL:temporaryRTCURL];
+        }
+    }
 }
 
 #pragma mark - Cheats -
@@ -159,5 +191,29 @@
     
 }
 
+#pragma mark - Private -
+
+- (BOOL)safelyCopyFileAtURL:(NSURL *)URL toURL:(NSURL *)destinationURL
+{
+    NSError *error = nil;
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:destinationURL.path])
+    {
+        if (![[NSFileManager defaultManager] removeItemAtPath:destinationURL.path error:&error])
+        {
+            NSLog(@"%@", error);
+            return NO;
+        }
+    }
+    
+    // Copy saves to ensure data is never lost.
+    if (![[NSFileManager defaultManager] copyItemAtURL:URL toURL:destinationURL error:&error])
+    {
+        NSLog(@"%@", error);
+        return NO;
+    }
+    
+    return YES;
+}
 
 @end
